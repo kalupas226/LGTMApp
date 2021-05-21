0caa79850ae0e94d1fd5ea1db40fb3b8dd666b55
Kenta Aikawa <kenta.aikawa@sansan.com>
2021-05-21 23:45:47 +0900

    Added: Sample Test も追加

--- Sources/Package.swift
+++ Sources/Package.swift
@@ -16,7 +16,18 @@ let package = Package(
         .package(url: "https://github.com/pointfreeco/swift-composable-architecture.git", from: "0.18.0"),
     ],
     targets: [
-        .target(name: "AppFeature", dependencies: []),
-        .testTarget(name: "AppFeatureTests", dependencies: []),
+        .target(
+            name: "AppFeature",
+            dependencies: [
+                .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+            ]
+        ),
+        .testTarget(
+            name: "AppFeatureTests",
+            dependencies: [
+                "AppFeature",
+                .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+            ]
+        ),
     ]
 )
--- Sources/Sources/AppFeature/AppView.swift
+++ Sources/Sources/AppFeature/AppView.swift
@@ -5,19 +5,56 @@
 //  Created by Aikawa Kenta on 2021/05/21.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
-public struct AppView: View {
+public struct AppState: Equatable {
     public init() {}
 
+    var testText = ""
+}
+
+public enum AppAction: Equatable {
+    case textFieldChanged(String)
+}
+
+public struct AppEnvironment: Equatable {
+    public init() {}
+}
+
+public let appReducer = Reducer<AppState, AppAction, AppEnvironment> { state, action, environment in
+    switch action {
+    case .textFieldChanged(let text):
+        state.testText = text
+        return .none
+    }
+}
+
+public struct AppView: View {
+    let store: Store<AppState, AppAction>
+
+    public init(store: Store<AppState, AppAction>) {
+        self.store = store
+    }
+
     public var body: some View {
-        Text("Hello, world!")
-            .padding()
+        WithViewStore(store) { viewStore in
+            TextField(
+                "Enter Text",
+                text: viewStore.binding(get: \.testText, send: AppAction.textFieldChanged)
+            )
+        }
     }
 }
 
 struct AppView_Previews: PreviewProvider {
     static var previews: some View {
-        AppView()
+        AppView(
+            store: Store(
+                initialState: AppState(),
+                reducer: appReducer,
+                environment: AppEnvironment()
+            )
+        )
     }
 }
