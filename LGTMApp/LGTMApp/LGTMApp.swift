5c7ee6f9c1381bb9fd8abde27bd819601eb050ef
Kenta Aikawa <kenta.aikawa@sansan.com>
2021-05-21 22:43:16 +0900

    Swift Package Maneger 構成でプロジェクトを作成した

---
+++ LGTMApp/LGTMApp/LGTMApp.swift
@@ -0,0 +1,18 @@
+//
+//  LGTMApp.swift
+//  LGTMApp
+//
+//  Created by Aikawa Kenta on 2021/05/21.
+//
+
+import AppFeature
+import SwiftUI
+
+@main
+struct LGTMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AppView()
+        }
+    }
+}
---
+++ Sources/Sources/AppFeature/AppView.swift
@@ -0,0 +1,23 @@
+//
+//  AppView.swift
+//  LGTMApp
+//
+//  Created by Aikawa Kenta on 2021/05/21.
+//
+
+import SwiftUI
+
+public struct AppView: View {
+    public init() {}
+
+    public var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct AppView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppView()
+    }
+}
